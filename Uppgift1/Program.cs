a8242f244ee76175c1b0809f70e667af913b323b
Pingvu <nhlpool27@gmail.com>
2023-09-07 13:17:38 +0200

    Add project files.

---
+++ Uppgift1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Uppgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1."); // den skriver ut text
+            Console.WriteLine("hej");
+        }
+    }
+}
